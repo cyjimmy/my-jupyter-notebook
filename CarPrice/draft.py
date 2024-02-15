0391cdc53c5fb7a46dac2284c8c0c8aa4cbe4ea7
Jimmy Wong <cy.jimmy.wong@gmail.com>
2024-02-15 14:08:05 -0800

    one hot encode

--- CarPrice/draft.py
+++ CarPrice/draft.py
@@ -1,24 +1,25 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 # Load the data
 df = pd.read_excel('./formattedData.xlsx')
-
-# Print info of the data
+# Print head of the dataset
 print(df.head())
-print(df.columns.to_list())
 
 # Find all column with non-numeric data
 non_numeric_columns = []
 for column in df.columns:
     if df[column].dtype == 'object':
         non_numeric_columns.append(column)
-
 # Print the non-numeric columns
 print(non_numeric_columns)
 
+# One-hot encode the non-numeric columns
+df = pd.get_dummies(df, columns=non_numeric_columns)
+# Print the head of the dataset
+print(df.head())
 
 # # Split the data into train and test
 # X = df.drop('Price', axis=1)
@@ -33,4 +34,5 @@ print(non_numeric_columns)
 # y_pred = model.predict(X_test)
 # mse = mean_squared_error(y_test, y_pred)
 # print(f'Mean Squared Error: {mse}')
-
+# mae = mean_absolute_error(y_test, y_pred)
+# print(f'Mean Absolute Error: {mae}')
