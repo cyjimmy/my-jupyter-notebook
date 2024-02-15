b65a35996a935be6317b8160ac8d2278a284aae8
Jimmy Wong <cy.jimmy.wong@gmail.com>
2024-02-15 15:45:25 -0800

    remove outliers

--- CarPrice/draft.py
+++ CarPrice/draft.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -9,32 +10,22 @@ df = pd.read_excel('./formattedData.xlsx')
 # Print head of the dataset
 print(df.head())
 
-# Plot histogram of numerical columns
-df.hist(figsize=(13, 10))
+# Identify all column with non-numeric data
+non_numeric_columns = ['Make', 'Model', 'Body_Type', 'Engine', 'Transmission', 'Drivetrain', 'Exterior_Colour', 'Interior_Colour', 'Fuel_Type']
+
+# Show the distribution of the numerical columns
+# df.hist(figsize=(12, 10))
+# plt.show()
+
+# Remove outliers from the numerical columns
+numeric_columns = df.drop(non_numeric_columns, axis=1).columns
+for column in numeric_columns:
+    df = df[(df[column] >= df[column].quantile(0.01)) & (df[column] <= df[column].quantile(0.99))]
+
+# Show new distribution of the numerical columns
+print(df.describe())
+df.hist(figsize=(12, 10))
 plt.show()
-
-
-# # Plot price distribution
-# plt.hist(df['Price'])
-# plt.title('Price')
-# plt.show()
-
-# # Remove upper 1% of the price
-# q = df['Price'].quantile(0.99)
-# df = df[df['Price'] < q]
-
-# # Plot price distribution
-# plt.hist(df['Price'])
-# plt.title('Price')
-# plt.show()
-
-# # Plot all columns distribution
-# df = df.astype(str)
-# for column in df.columns:
-#     print(column)
-#     plt.hist(df[column])
-#     plt.title(column)
-#     plt.show()
 
 # # Find all column with non-numeric data
 # non_numeric_columns = []
