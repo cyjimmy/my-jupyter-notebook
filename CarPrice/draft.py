8fe31dfc825dcd5bef3406c2a3103ab459c3c82f
Jimmy Wong <cy.jimmy.wong@gmail.com>
2024-02-15 14:47:56 -0800

    add histogram plot

--- CarPrice/draft.py
+++ CarPrice/draft.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -8,18 +9,45 @@ df = pd.read_excel('./formattedData.xlsx')
 # Print head of the dataset
 print(df.head())
 
-# Find all column with non-numeric data
-non_numeric_columns = []
-for column in df.columns:
-    if df[column].dtype == 'object':
-        non_numeric_columns.append(column)
-# Print the non-numeric columns
-print(non_numeric_columns)
+# Plot histogram of numerical columns
+df.hist(figsize=(13, 10))
+plt.show()
 
-# One-hot encode the non-numeric columns
-df = pd.get_dummies(df, columns=non_numeric_columns)
-# Print the head of the dataset
-print(df.head())
+
+# # Plot price distribution
+# plt.hist(df['Price'])
+# plt.title('Price')
+# plt.show()
+
+# # Remove upper 1% of the price
+# q = df['Price'].quantile(0.99)
+# df = df[df['Price'] < q]
+
+# # Plot price distribution
+# plt.hist(df['Price'])
+# plt.title('Price')
+# plt.show()
+
+# # Plot all columns distribution
+# df = df.astype(str)
+# for column in df.columns:
+#     print(column)
+#     plt.hist(df[column])
+#     plt.title(column)
+#     plt.show()
+
+# # Find all column with non-numeric data
+# non_numeric_columns = []
+# for column in df.columns:
+#     if df[column].dtype == 'object':
+#         non_numeric_columns.append(column)
+# # Print the non-numeric columns
+# print(non_numeric_columns)
+
+# # One-hot encode the non-numeric columns
+# df = pd.get_dummies(df, columns=non_numeric_columns)
+# # Print the head of the dataset
+# print(df.head())
 
 # # Split the data into train and test
 # X = df.drop('Price', axis=1)
