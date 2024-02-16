ba204c8cf430299c2ba61c3704c05cc78e1b733b
Jimmy Wong <cy.jimmy.wong@gmail.com>
2024-02-15 19:20:25 -0800

    train and evaluate model

--- CarPrice/draft.py
+++ CarPrice/draft.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_absolute_error
 
 # Load the data
 df = pd.read_excel('./formattedData.xlsx')
@@ -24,34 +24,32 @@ for column in numeric_columns:
 
 # Show new distribution of the numerical columns
 print(df.describe())
-df.hist(figsize=(12, 10))
-plt.show()
+# df.hist(figsize=(12, 10))
+# plt.show()
 
-# # Find all column with non-numeric data
-# non_numeric_columns = []
-# for column in df.columns:
-#     if df[column].dtype == 'object':
-#         non_numeric_columns.append(column)
-# # Print the non-numeric columns
-# print(non_numeric_columns)
+# One-hot encode the non-numeric columns
+df = pd.get_dummies(df, columns=non_numeric_columns)
+# Print the head of the dataset
+print(df.head())
 
-# # One-hot encode the non-numeric columns
-# df = pd.get_dummies(df, columns=non_numeric_columns)
-# # Print the head of the dataset
-# print(df.head())
+# Split the data into train and test
+X = df.drop('Price', axis=1)
+y = df['Price']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# # Split the data into train and test
-# X = df.drop('Price', axis=1)
-# y = df['Price']
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# # Train the model
+# Train the model
 # model = LinearRegression()
-# model.fit(X_train, y_train)
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
 
-# # Evaluate the model
-# y_pred = model.predict(X_test)
-# mse = mean_squared_error(y_test, y_pred)
-# print(f'Mean Squared Error: {mse}')
-# mae = mean_absolute_error(y_test, y_pred)
-# print(f'Mean Absolute Error: {mae}')
+# Evaluate the model
+y_pred = model.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+print(f'Mean Absolute Error: {mae}')
+
+# Plot acutal vs predicted
+plt.scatter(y_test, y_pred)
+plt.xlabel('Actual Price')
+plt.ylabel('Predicted Price')
+plt.title('Actual vs Predicted Price')
+plt.show()
